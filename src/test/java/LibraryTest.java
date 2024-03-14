dc033c1892d75030bcd7f46bdfd468132d6b351b
Rishabh Singh <rishabhksingh@gmail.com>
2024-03-13 19:31:57 -0700

    Update LibraryTest.java

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -12,11 +12,11 @@ import static org.junit.Assert.*;
 public class LibraryTest {
     @Test public void testSomeLibraryMethod() {
         Library classUnderTest = new Library();
-        assertFalse("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
     }
     
     @Test public void testNewLibraryMethod() {
         Library classUnderTest = new Library();
-        assertTrue("someLibraryMethod should return 'false'", classUnderTest.newLibraryMethod());
+        assertFalse("someLibraryMethod should return 'false'", classUnderTest.newLibraryMethod());
     }
 }
